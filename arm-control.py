1505c3acb23635bc5934d0857304e730913213ba
y <t>
2019-10-11 18:44:24 +0000

    yes

--- arm-control.py
+++ arm-control.py
@@ -10,10 +10,12 @@ class BluetoothManager (object):
         self.peripheral = None
 
     def did_discover_peripheral(self, p):
-        if p.name and not self.peripheral:
-            self.peripheral = p
-            print('Connecting...')
-            cb.connect_peripheral(p)
+        if p.name:
+            if p.name == "HMSoft" or p.name == "Test":
+                print(p.name)
+                self.peripheral = p
+                print('Connecting...')
+                cb.connect_peripheral(p)
 
     def did_connect_peripheral(self, p):
         print('Connected:', p.name)
@@ -29,15 +31,17 @@ class BluetoothManager (object):
 
     def did_discover_services(self, p, error):
         for s in p.services:
-            if s.uuid == '180D':
-                print('Discovered heart rate service, discovering characteristitcs...')
+            print(s.uuid)
+            if (s.uuid == 'FFE0'):
+                print('Service discovered')
                 p.discover_characteristics(s)
 
     def did_discover_characteristics(self, s, error):
         print('Did discover characteristics...')
         for c in s.characteristics:
-            if c.uuid == '2A37':
-                self.peripheral.set_notify_value(c, True)
+            print(c.uuid)
+            if c.uuid == 'FFE1':
+                self.peripheral.write_characteristic_value(c, 'test', False)
 
     def did_update_value(self, c, error):
         heart_rate = struct.unpack('<B', c.value[1])[0]
